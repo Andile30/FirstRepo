d56cecb524793b7587407a58f53c998cb12322ca
Andile <andilezulu200406@gmail.com>
2024-05-10 14:27:18 +0200

    First Change

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Your Name: ");
+        String name = input.next();
+        System.out.println("Welcome " + name);
         System.out.println("Hello world!");
+
     }
+
 }
